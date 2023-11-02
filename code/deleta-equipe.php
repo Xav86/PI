e67324eae6433f0b954628eb638a59da4a94c4f1
Xav86 <gustavo.g.2005@gmail.com>
2023-11-02 18:31:13 -0300

    criada uma tela para alertar o sucesso da operação

--- code/deleta-equipe.php
+++ code/deleta-equipe.php
@@ -7,9 +7,39 @@ $sql = "UPDATE equipes SET statu='desativo' WHERE id_equipe='$pk'";
 
 $res = mysqli_query($id, $sql);
 
-if ($res) {
-    echo "Registro excluido com sucesso";
-} else {
+if ($res) { ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro Bem-sucedido</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <div style="text-align: center;">
+        <h1>Excluido com sucesso</h1>
+        <p style="color:lightslategray;"><small><i>este cadastro foi desativado, caso queira ativalo contate um adiministrador</i></small></p>
+    </div>
+
+    <script>
+        setTimeout(function() {
+            window.location.href = "../visualiza_equipe.php"; 
+        }, 4000); 
+    </script>
+</body>
+</html>
+<?php 
+} 
+else 
+{
     echo "Erro ao excluir o registro do banco de dados";
 }
 ?>
