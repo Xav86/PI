b0070ef288796495770c908ec3b3f60ebf5143f0
Xav86 <gustavo.g.2005@gmail.com>
2023-11-08 20:19:58 -0300

    realizado cadastro da prova e pegado valor da chave estrangeira

--- code/cadastra-prova.php
+++ code/cadastra-prova.php
@@ -10,5 +10,23 @@ $ponto5 = $_POST['ponto5'];
 $nome = $_POST['nome'];
 $desc = $_POST['descricao'];
 
-echo "$numero, $ponto1, $ponto2, $ponto3, $ponto4, $ponto5, $nome, $desc"
+$sql = "INSERT INTO provas (numero,nome,descricao) values ('$numero','$nome','$desc')";
+
+$res = mysqli_query($id, $sql);
+
+$sql = "SELECT MAX(id_prova) from provas";
+$pesquisa = mysqli_query($id, $sql);
+$dados = mysqli_fetch_array($pesquisa);
+$cod = $dados[0];
+
+$sql2 = "INSERT INTO pontos (cod_prova,ponto1,ponto2,ponto3,ponto4,ponto5) values ($cod,'$ponto1','$ponto2','$ponto3','$ponto4','$ponto5')";
+
+$res2 = mysqli_query($id, $sql2);
+
+if (($res) && ($res2)) {
+    echo "Cadastrado com sucesso";
+} else {
+    echo "Erro ao cadastrar " . mysqli_error($id);
+}
+
 ?>
