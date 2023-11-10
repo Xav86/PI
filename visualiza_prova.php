9165197b05143eea7b3578545537883560087840
Xav86 <gustavo.g.2005@gmail.com>
2023-11-09 21:54:00 -0300

    começando tela de visualização de provas

--- visualiza_prova.php
+++ visualiza_prova.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/style-home.css">
-    <title>Menu Principal</title>
+    <link rel="stylesheet" href="css/style-visualiza-prova.css">
+    <title>Visualiza Prova</title>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </head>
-
 <body>
     <!-- Barra de Navegação -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -76,9 +75,58 @@
         </div>
         
     </nav>
-
+ 
     <main>
-        <h1>você esta no menu</h1>     
+            <div id="tabela">
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">N° da prova</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col" >Ponto 1</th>
+                            <th scope="col" >Ponto 2</th>
+                            <th scope="col" >Ponto 3</th>
+                            <th scope="col" >Ponto 4</th>
+                            <th scope="col" >Ponto 5</th>
+                            <th scope="col" >Descrição</th>
+                        </tr>
+                    </thead>
+                    <?php
+                        require 'code/connection.php';
+
+                        $sql = ('SELECT * FROM provas');
+                        $sql2 = ('SELECT * FROM pontos');
+
+                        $res = mysqli_query($id, $sql, $sql2);
+                        $posicao = 0;
+                        while ($linha = mysqli_fetch_array($res)) { 
+
+                            $posicao = $posicao+1;
+                            ?>
+                        
+                            <tr>
+                                <td>
+                                    <?php echo $posicao;?>
+                                </td>
+                                <td>
+                                    <?php echo $linha[''];?>
+                                </td>
+                                <td>
+                                    <?php echo $linha[''];?>
+                                </td>
+                                <td>
+                                    <?php echo $linha[''];?>     
+                                </td>
+                
+                            </tr>
+                
+                    <?php  } ?>
+                </table>
+                
+            </div> 
+
     </main>
 
 </body>
