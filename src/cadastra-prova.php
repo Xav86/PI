92136825bbfca3bc8f132b476a6213341974203d
Xav86 <gustavo.g.2005@gmail.com>
2023-11-11 19:21:04 -0300

    feito cadastro de prova com novo banco

--- src/cadastra-prova.php
+++ src/cadastra-prova.php
@@ -5,21 +5,20 @@ $numero = $_POST['numero'];
 $ponto1 = $_POST['ponto1'];
 $ponto2 = $_POST['ponto2'];
 $ponto3 = $_POST['ponto3'];
-$ponto4 = $_POST['ponto4'];
-$ponto5 = $_POST['ponto5'];
+$padrao = $_POST['padrao'];
 $nome = $_POST['nome'];
 $desc = $_POST['descricao'];
 
-$sql = "INSERT INTO provas (numero,nome,descricao) values ('$numero','$nome','$desc')";
+$sql = "INSERT INTO pontuacao (primeiro,segundo,terceiro,ponto_padrao) values ('$ponto1','$ponto2','$ponto3','$padrao')";
 
 $res = mysqli_query($id, $sql);
 
-$sql = "SELECT MAX(id_prova) from provas";
+$sql = "SELECT MAX(id) from pontuacao";
 $pesquisa = mysqli_query($id, $sql);
 $dados = mysqli_fetch_array($pesquisa);
 $cod = $dados[0];
 
-$sql2 = "INSERT INTO pontos (cod_prova,ponto1,ponto2,ponto3,ponto4,ponto5) values ($cod,'$ponto1','$ponto2','$ponto3','$ponto4','$ponto5')";
+$sql2 = "INSERT INTO provas (nome,descricao,numero_prova,pontuacao_id) values ('$nome','$desc','$numero','$cod')";
 
 $res2 = mysqli_query($id, $sql2);
 
